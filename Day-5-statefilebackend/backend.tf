6c4a0ae1acbfeea5a493e19443dcea4f1c70d907
ameya30a <ameyabhandakkar9@gmail.com>
2025-07-24 12:10:15 +0530

    Deleted Day-5 folder

--- Day-5-statefilebackend/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-bucketettt"
-    key    = "path/to/my/key"
-    region = "us-east-1"
-  }
-}
--- Day-5-statefilebackend/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_instance" "name" {
-  ami           = "ami-05ffe3c48a9991133"   # Your AMI ID
-  instance_type = "t2.micro"
-  key_name      = "my-linux"
-  subnet_id     = "subnet-0129ac093f48da17a"  # 👈 Ye line add karo
-}
-
