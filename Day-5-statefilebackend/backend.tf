712595b909687713010128166090afc3183207a1
ameya30a <ameyabhandakkar9@gmail.com>
2025-07-25 12:08:12 +0530

    update

--- Day-5-statefilebackend/backend.tf
+++ Day-5-statefilebackend/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket = "terraforn-bucketttteeee"
     key    = "terraform.tfstate"
     region = "us-east-1"
-    use_lockfile = true
+    # use_lockfile = true
     dynamodb_table = "test"
     encrypt = true
   }
